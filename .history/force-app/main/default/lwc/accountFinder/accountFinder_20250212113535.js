8100400a82937f5dbabdd0345d30b5798391364a
marjuta <108944898+marjuta@users.noreply.github.com>
2025-02-12 11:52:26 +0100

    https://trailhead.salesforce.com/de/content/learn/modules/lwc-for-visualforce-developers 12.03.25

--- .history/force-app/main/default/lwc/accountFinder/accountFinder_20250212113535.js
+++ .history/force-app/main/default/lwc/accountFinder/accountFinder_20250212113535.js
@@ -1,7 +1,5 @@
 import { LightningElement, wire } from 'lwc';
 import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
-
-
 export default class AccountFinder extends LightningElement {
     annualRevenue = null;
     handleChange(event) {
@@ -12,5 +10,4 @@ export default class AccountFinder extends LightningElement {
     }
      @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue' })
     accounts;
-    
 }
